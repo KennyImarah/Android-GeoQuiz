79a903d9114275541c2ced66756c0fa25f5df7d1
Kenny Imarah <kenny.o.imarah@gmail.com>
2020-02-23 22:27:56 -0500

    cheatActivity added

--- app/src/main/java/com/example/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/geoquiz/CheatActivity.java
@@ -2,10 +2,14 @@ package com.example.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -49,9 +53,26 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+
+                //Android Version compatibility condition
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                    int cx = mShowAnswerButton.getWidth()/2;
+                    int cy = mShowAnswerButton.getHeight()/2;
+                    float radius = mShowAnswerButton.getWidth();
+                    Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
             }
         });
-
     }
     private void setAnswerShownResult(boolean isAnswerShown) {
         Intent data = new Intent();
