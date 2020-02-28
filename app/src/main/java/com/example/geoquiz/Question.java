5bbffff88e66fe689e2808c1f9d46437648ab507
Kenny Imarah <kenny.o.imarah@gmail.com>
2020-02-27 20:41:07 -0500

    Updated and working

--- app/src/main/java/com/example/geoquiz/Question.java
+++ app/src/main/java/com/example/geoquiz/Question.java
@@ -1,23 +1,29 @@
 package com.example.geoquiz;
 
+//Question class
 public class Question {
+    //TextField variable
     private int mTextResId;
+    //AnswerTrue variable
     private boolean mAnswerTrue;
 
+    //Question constructor
     public Question(int mTextResId, boolean mAnswerTrue){
         this.mTextResId = mTextResId;
         this.mAnswerTrue = mAnswerTrue;
     }
 
+    //getter and setter for fields
     public int getTextResId() {
         return mTextResId;
     }
 
-
+    //mTextResId value assigned to textResId
     public void setTextResId(int textResId) {
         mTextResId = textResId;
     }
 
+    //method for isAnswerTrue
     public boolean isAnswerTrue() {
         return mAnswerTrue;
     }
